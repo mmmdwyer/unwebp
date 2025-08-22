5094c1475df12cc981f688bbbb7ce5bb6ac8ec94
Michael Dwyer <mdwyer@timestreamtech.com>
2025-08-21 17:20:03 -0700

    Vastly simplified the decision tree.
    Originally, I took a lot of size measurements to try to decide
    if I should use PNG or JPEG. But it turns out you should pretty
    much always use JPEG unless the webp is lossless or uses features
    that aren't in JPEG like alpha channels or animation.
    So the code does only that.

--- unwebp.pl
+++ unwebp.pl
@@ -16,23 +16,19 @@ for entry in os.scandir('.'):
 			print(f'Could not open file: {entry.name}')
 			continue
 
-		filename = image.filename
-		print(f'-- Inspecting {filename}')
+		print(f'-- Inspecting {image.filename}')
 		if image.format != "WEBP":
-			print(f'File is {image.format}, not a WEBP file: {filename}')
+			print(f'File is {image.format}, not a WEBP file: {image.filename}')
 			continue
 		# Collecting some assorted info
 		exif = image.getexif()
-		dimensions = f'{image.width}x{image.height}'
-		webplength = os.stat(filename).st_size
-		rawlength = len(image.tobytes())
 
 		# Set flags
 		force_png = False
 
 		# Try out PNG encoding
 		pngversion = io.BytesIO()
-		with open(filename, "rb") as fp:
+		with open(image.filename, "rb") as fp:
 			if fp.read(15)[-1:] == b"L":
 				# VP8L means lossless. Look for that L
 				print("*** Lossless detected. Forcing PNG.")
@@ -46,31 +42,15 @@ for entry in os.scandir('.'):
 			print("*** Alpha channel detected. Forcing PNG.")
 			force_png = True
 			image.save(pngversion, 'PNG')
-		else:
-			image.save(pngversion, 'PNG')
-			force_png = True
-		pnglength = len(pngversion.getbuffer())
-
+			
 		# At this point, I think we've selected PNG for any images
 		# that have a hard requirement for it. What's left are 
 		# almost certainly supposed to be JPEGs.
-
-		# Try out JPG encoding
-		jpglength = 0
-		if not force_png:
-			jpgversion = io.BytesIO()
-			image.save(jpgversion, 'JPEG', exif=exif)
-			jpglength = len(jpgversion.getbuffer())
-
-		# Do some math to help the decision
-		# I turned this off because I couldn't find any reasons 
-		# aside from what I'd already found to go webp->png.
-		#webppercent = (webplength * 100) / rawlength
-		#jpegpercent = (jpglength * 100) / rawlength
-		#pngpercent = (pnglength * 100) / rawlength
-		#print(f'{image.width}x{image.height} rawlen:{rawlength} webp:{webplength}({webppercent:.0f})  jpg:{jpglength}({jpegpercent:.0f}) png:{pnglength}({pngpercent:.0f})  {image.filename}')
-		#print(f'   jpgdiff:{jpegpercent-webppercent:.1f}')
-		#print(f'   pngdiff:{pngpercent-webppercent:.1f}')
-		#if (pngpercent-webppercent) < 10 :
-		#	print("*********")
-
+		if force_png:
+			outfilename = image.filename.removesuffix('.webp')+'.png'
+			with open(outfilename, 'wb') as fp:
+				fp.write(pngversion.getbuffer())
+		else:
+			outfilename = image.filename.removesuffix('.webp')+'.jpg'
+			image.save(outfilename, 'JPEG', exif=exif)
+		os.unlink(image.filename)
